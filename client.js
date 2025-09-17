aaa4fad6ca9a531e47aafbda794af2e0c8385464
andrewwbaranoff-ship-it <andrewwbaranoff@gmail.com>
2025-09-17 22:58:22 +0300

    Add files via upload

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 // Укажи здесь URL своего сигнального сервера
-const SIGNALING_SERVER = "https://PUT-YOUR-SIGNAL-SERVER-URL";
+const SIGNALING_SERVER = "https://akbserver.onrender.com";
 const socket = io(SIGNALING_SERVER, { transports: ['websocket'] });
 
 // Тут нужно вставить остальной JS код (обработчики, WebRTC логика) который я описывал выше.

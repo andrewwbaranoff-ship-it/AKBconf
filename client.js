d1ae632220b82f3c8b13bda56a3d0449224369bc
andrewwbaranoff-ship-it <andrewwbaranoff@gmail.com>
2025-09-17 22:51:46 +0300

    Add files via upload

---
+++ client.js
@@ -0,0 +1,6 @@
+// Укажи здесь URL своего сигнального сервера
+const SIGNALING_SERVER = "https://PUT-YOUR-SIGNAL-SERVER-URL";
+const socket = io(SIGNALING_SERVER, { transports: ['websocket'] });
+
+// Тут нужно вставить остальной JS код (обработчики, WebRTC логика) который я описывал выше.
+// Чтобы не перегружать здесь, предполагаем, что основной код client.js будет вставлен вами.
